48edfbd9d60f91a0093fcd5947966fbc44a9666e
JustForSmiling <1792626359@qq.com>
2018-12-14 16:32:42 +0800

    add sth

--- src/main.js
+++ src/main.js
@@ -6,13 +6,16 @@ import router from './router'
 import "@/assets/css/reset.css"
 import "@/assets/js/base.js"
 
-
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
+
+// require("./mock.js")
+require('./mock.js')
+
+
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
--- src/pages/index/v-index.vue
+++ src/pages/index/v-index.vue
@@ -9,6 +9,13 @@
 <script type="text/ecmascript-6">
 import VNav from "@/pages/common/v-nav";
 import "@/assets/css/main.styl";
+import axios from "axios";
+
+// jsonp用
+import Vue from "vue";
+import VueJsonp from "vue-jsonp";
+Vue.use(VueJsonp);
+
 export default {
     name: "",
     data() {
@@ -16,6 +23,25 @@ export default {
     },
     components: {
         VNav
+    },
+    created() {
+        // 测试mockjs用
+        // axios.get("http://localhost/php/index.php?imgSrc=guess").then(res => {
+        //     console.log(res.data);
+        // });
+        // 测试跨域请求代理用(配合config文件夹下index.js中proxyTable进行配置)
+        // axios
+        //     .post("/api/php/index.php?field=imgSrc&mark=guess")
+        //     .then(function(response) {
+        //         console.log(response);
+        //     });
+
+        // 测试jsonp用(不需要传callback函数, 内部已封装好)
+        this.$jsonp(
+            "https://sug.so.360.cn/suggest?word=a&encodein=utf-8&encodeout=utf-8&pq=&t=2574587"
+        ).then(res => {
+            console.log(res);
+        });
     }
 };
 </script>
